0e73947d7331f5ff5c5284e94beabce9beef0953
Roosterseatyou <ltbkthree@icloud.com>
2022-08-13 02:04:39 -0500

    Signs are now UwUed when typed on (config option included)

--- src/main/java/ml/treecaptcha/uwuify/uwuHandler.java
+++ src/main/java/ml/treecaptcha/uwuify/uwuHandler.java
@@ -2,6 +2,8 @@ package ml.treecaptcha.uwuify;
 import io.github.ran.uwu.client.Uwuifier;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.AsyncPlayerChatPreviewEvent;
 
@@ -15,6 +17,15 @@ public class uwuHandler implements Listener{
     public void onPlayerPreview(AsyncPlayerChatPreviewEvent event) {
         if(!Uwuify.uwu.getConfig().getBoolean("use-chat-preview")) return;
         event.setMessage(Uwuifier.uwuify(event.getMessage()));
+    }
+
+    @EventHandler
+    public void onPlayerSign(SignChangeEvent ev) {
+        if(!Uwuify.uwu.getConfig().getBoolean("signs-uwuify")) return;
+        for(int i = 0; i < ev.getLines().length; i++) {
+            if(ev.getLine(i) == null) continue;
+            ev.setLine(i, Uwuifier.uwuify(ev.getLine(i)));
+        }
     }
 
     public uwuHandler(Uwuify plugin){
