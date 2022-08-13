96aa3a09d59179ad96604076bdf70af7fd33b9a8
Roosterseatyou <ltbkthree@icloud.com>
2022-08-13 01:29:31 -0500

    allow for chat previewing stuffs

--- src/main/java/ml/treecaptcha/uwuify/uwuHandler.java
+++ src/main/java/ml/treecaptcha/uwuify/uwuHandler.java
@@ -3,16 +3,20 @@ import io.github.ran.uwu.client.Uwuifier;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
-import java.util.logging.Level;
-
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.AsyncPlayerChatPreviewEvent;
 
 public class uwuHandler implements Listener{
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         event.setMessage(Uwuifier.uwuify(event.getMessage()));
     }
+
+    @EventHandler
+    public void onPlayerPreview(AsyncPlayerChatPreviewEvent event) {
+        if(!Uwuify.uwu.getConfig().getBoolean("use-chat-preview")) return;
+        event.setMessage(Uwuifier.uwuify(event.getMessage()));
+    }
+
     public uwuHandler(Uwuify plugin){
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
