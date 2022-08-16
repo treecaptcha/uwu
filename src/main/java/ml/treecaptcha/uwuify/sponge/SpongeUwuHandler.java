2d93ad7d2b74dbf26a6959a81c3adfaf7404eb67
TreeCaptcha <70340775+treecaptcha@users.noreply.github.com>
2022-08-15 22:17:40 -0400

    Cleanup

--- src/main/java/ml/treecaptcha/uwuify/sponge/SpongeUwuHandler.java
+++ src/main/java/ml/treecaptcha/uwuify/sponge/SpongeUwuHandler.java
@@ -1,11 +1,13 @@
 package ml.treecaptcha.uwuify.sponge;
 
+import io.github.ran.uwu.client.Uwuifier;
+
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.message.MessageEvent;
-import io.github.ran.uwu.client.Uwuifier;
+
 public class SpongeUwuHandler {
     @Listener
-    public void onMessage(MessageEvent event){
+    public void onMessage(MessageEvent event) {
         Uwuify.lagger.info(Uwuifier.uwuify("Got message!"));
     }
 }
