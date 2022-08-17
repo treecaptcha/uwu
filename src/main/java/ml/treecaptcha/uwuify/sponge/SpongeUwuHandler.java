725d63cec932110ae1946dab0ed8fc558b96d2d1
TreeCaptcha <70340775+treecaptcha@users.noreply.github.com>
2022-08-16 22:57:56 -0400

    Sponge is ready without config

--- src/main/java/ml/treecaptcha/uwuify/sponge/SpongeUwuHandler.java
+++ src/main/java/ml/treecaptcha/uwuify/sponge/SpongeUwuHandler.java
@@ -5,10 +5,11 @@ import io.github.ran.uwu.client.Uwuifier;
 import ml.treecaptcha.uwuify.core.AdventureChat;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.message.MessageEvent;
+import org.spongepowered.api.event.message.PlayerChatEvent;
 
 public class SpongeUwuHandler {
     @Listener
-    public void onMessage(MessageEvent event) {
+    public void onMessage(PlayerChatEvent event) {
 
         event.setMessage(
                 AdventureChat.twoComponent(
