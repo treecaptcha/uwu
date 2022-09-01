2f95e8ffac252669a39ec29a41439c0467f3fa19
TreeCaptcha <70340775+treecaptcha@users.noreply.github.com>
2022-08-31 23:50:18 -0400

    fix logic (part 2)

--- src/main/java/ml/treecaptcha/uwuify/paper/PaperUwuHandler.java
+++ src/main/java/ml/treecaptcha/uwuify/paper/PaperUwuHandler.java
@@ -15,15 +15,12 @@ import org.bukkit.inventory.meta.BookMeta;
 public class PaperUwuHandler implements Listener {
 
 
-
-
-
     @EventHandler
     public void onSignChange(org.bukkit.event.block.SignChangeEvent e) {
-        if(!Uwuify.SIGNS_UWUIFY) return;
+        if (!Uwuify.SIGNS_UWUIFY) return;
         PlainTextComponentSerializer serializer = PlainTextComponentSerializer.plainText();
-        for(int i = 0; i < e.lines().size(); i++) {
-            if("".equals(serializer.serialize(e.lines().get(i)))) continue;
+        for (int i = 0; i < e.lines().size(); i++) {
+            if ("".equals(serializer.serialize(e.lines().get(i)))) continue;
 
             e.line(i, Component.text(Uwuifier.uwuifyMessage(serializer.serialize(e.lines().get(i)))));
         }
@@ -31,14 +28,14 @@ public class PaperUwuHandler implements Listener {
 
     @EventHandler
     public void onBook(PlayerEditBookEvent e) {
-        if(!Uwuify.BOOKS_UWUIFY) return;
+        if (!Uwuify.BOOKS_UWUIFY) return;
         BookMeta meta = e.getNewBookMeta();
 
-        for(int i = 0; i < meta.getPageCount(); i++) {
-            meta.page(i+1, Component.text(Uwuifier.uwuify(AdventureChat.twoString(meta.page(i+1)))));
+        for (int i = 0; i < meta.getPageCount(); i++) {
+            meta.page(i + 1, Component.text(Uwuifier.uwuify(AdventureChat.twoString(meta.page(i + 1)))));
         }
 
-        if(meta.title() != null) {
+        if (meta.title() != null) {
             meta.title(Component.text(Uwuifier.uwuify(AdventureChat.twoString(meta.title()))));
         }
         e.setNewBookMeta(meta);
@@ -46,18 +43,20 @@ public class PaperUwuHandler implements Listener {
 
     @EventHandler
     public void onEntityName(PlayerNameEntityEvent e) {
-        if(!Uwuify.ANIMALS_UWUIFY) return;
+        if (!Uwuify.ANIMALS_UWUIFY) return;
         e.setName(Component.text(Uwuifier.uwuifyMessage(AdventureChat.twoString(e.getName()))));
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        if(!Uwuify.PLAYER_NAMES_UWUIFY) return;
-        event.getPlayer().displayName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
-        event.getPlayer().customName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
-        event.getPlayer().playerListName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
-        event.getPlayer().setCustomNameVisible(true);
-        if(!Uwuify.JOIN_MESSAGES_UWUIFY) return;
-        event.joinMessage(Component.text(Uwuifier.uwuifyMessage(AdventureChat.twoString(event.joinMessage()))));
+        if (Uwuify.PLAYER_NAMES_UWUIFY) {
+            event.getPlayer().displayName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
+            event.getPlayer().customName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
+            event.getPlayer().playerListName(Component.text(Uwuifier.uwuifyName(AdventureChat.twoString(event.getPlayer().displayName()))));
+            event.getPlayer().setCustomNameVisible(true);
+        }
+        if (Uwuify.JOIN_MESSAGES_UWUIFY) {
+            event.joinMessage(Component.text(Uwuifier.uwuifyMessage(AdventureChat.twoString(event.joinMessage()))));
+        }
     }
 }
